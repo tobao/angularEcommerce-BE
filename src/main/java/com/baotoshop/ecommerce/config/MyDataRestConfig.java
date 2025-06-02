c330d0801e00dfa6f3c38f9beb37624de9919f90
BaoTo <tobao97@gmail.com>
2025-06-02 23:44:54 +0700

    Refactoring Backend Configurations

--- src/main/java/com/baotoshop/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/baotoshop/ecommerce/config/MyDataRestConfig.java
@@ -7,6 +7,7 @@ import com.baotoshop.ecommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -19,6 +20,9 @@ import java.util.Set;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
+
     private final EntityManager entityManager;
 
     @Autowired
@@ -38,6 +42,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         // call an internal helper method
         exposeIds(config);
+
+        // configure cors mapping
+        //cors.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
 
     }
 
--- src/main/java/com/baotoshop/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/baotoshop/ecommerce/controller/CheckoutController.java
@@ -5,7 +5,6 @@ import com.baotoshop.ecommerce.dto.PurchaseResponse;
 import com.baotoshop.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
